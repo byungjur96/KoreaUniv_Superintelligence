424093ec3e7592e6229ba3883e39abd776946483
byungjur96 <cbyungjub@naver.com>
2023-12-11 12:59:31 +0000

    Shell file update

--- train.sh
+++ train.sh
@@ -1,18 +1,15 @@
-export MODEL_NAME="CompVis/stable-diffusion-v1-4"
-export DATASET_NAME="./train"
-
-CUDA_VISIBLE_DEVICES=1
-
-accelerate launch --mixed_precision="fp16" train_text_to_image.py \
-  --pretrained_model_name_or_path=$MODEL_NAME \
-  --train_data_dir="./train" \
+accelerate launch train_text_to_image.py \
+  --pretrained_model_name_or_path="CompVis/stable-diffusion-v1-4" \
+  --train_data_dir="../Dataset/train" \
   --use_ema \
   --resolution=512 --center_crop --random_flip \
   --train_batch_size=1 \
   --gradient_accumulation_steps=4 \
   --gradient_checkpointing \
+  --mixed_precision="fp16" \
   --max_train_steps=15000 \
   --learning_rate=1e-05 \
   --max_grad_norm=1 \
   --lr_scheduler="constant" --lr_warmup_steps=0 \
-  --output_dir="krlee"
+  --output_dir="../Quilt_CLIP"
+  
